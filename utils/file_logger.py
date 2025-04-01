b0187af17db190f02785215fe55823b9ffb1e839
ybvy <buivanyk4@gmail.com>
2025-04-01 21:48:24 +0700

    fix file_logger.py

--- utils/file_logger.py
+++ utils/file_logger.py
@@ -1,3 +1,18 @@
+# -*- coding: utf-8 -*-
+#  psdir - Web Path Scanner
+#  Copyright (C) 2025 waibui
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  Author: waibui
+
 import json
 import csv
 import yaml
