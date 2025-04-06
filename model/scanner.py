7a6d3ec5a1c99282c6aa3f8ae375dce429868241
ybvy <buivanyk4@gmail.com>
2025-04-06 16:33:39 +0700

    fix header

--- model/scanner.py
+++ model/scanner.py
@@ -19,10 +19,6 @@
 #  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #  SOFTWARE.
-#
-#  Author: waibui
-#  Email: waibui@example.com
-#  Website: https://github.com/waibui
 
 import asyncio
 import aiohttp
